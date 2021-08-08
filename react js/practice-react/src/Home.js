ad7dd6486890e9f349f0f3f402b45231e27dc630
Fikri <fikrichannel120@gmail.com>
2021-08-08 23:43:46 +0700

    create props in practice-react

--- react js/practice-react/src/Home.js
+++ react js/practice-react/src/Home.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 
-const Home = props => {
+const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My New Website', body: 'loremmmasdma', author:'mario', id:'1' },
         { title: 'Welcome Party', body: 'loremmmasdma', author:'fikri', id:'2' },
