efaec47e061b8cb1abac06c480764a802fbae539
Muhammad Nur Fikri <mf4853432@gmail.com>
2021-08-08 15:24:55 +0700

    mencoba membuat class component dalam 1 file

--- react js/practice-react/src/App.js
+++ react js/practice-react/src/App.js
@@ -1,12 +1,24 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    
-     <h1 className="App">Hello Word!</h1>
-    
-  );
+class Header extends Component{
+  render(){
+    return(
+    <div>
+      <h2>Makanan Indonesia</h2>
+       </div>
+       );
+  }
 }
-
+class App extends Component{
+  render(){
+    return(
+    <div>
+    <Header />
+     <h1 className="App">Hello Word!</h1>
+     </div>
+    );
+}
+}
 export default App;
